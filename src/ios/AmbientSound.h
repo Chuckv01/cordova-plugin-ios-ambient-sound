56ccfcca3d519ba01b033149518353f96b3cb900
Chuckv01 <charles.vanderhoff@gmail.com>
2017-01-24 08:54:49 -0800

    Update AmbientSound.h

--- src/ios/AmbientSound.h
+++ src/ios/AmbientSound.h
@@ -1,26 +1,6 @@
-/**
- * Cordova (iOS) plugin for setting the ambient sound setting on device.
- */
+#import <Cordova/CDV.h>
+#import "AppDelegate.h"
 
-#ifdef CORDOVA_FRAMEWORK
-#import <CORDOVA/CDVPlugin.h>
-#else
-#import "CORDOVA/CDVPlugin.h"
-#endif
-
-/**
- * Interface which does the actual handling
- */
-@interface AmbientSound :CDVPlugin {    
-}
-/**
- * Sets the idleTimerDisable property to true so that the idle timeout is disabled
- */
-- (void) acquire:(CDVInvokedUrlCommand*)command;
-
-/**
- * Sets the idleTimerDisable property to false so that the idle timeout is enabled
- */
-- (void) release:(CDVInvokedUrlCommand*)command;
+@interface AmbientSound : CDVPlugin
 
 @end
